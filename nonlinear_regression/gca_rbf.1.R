91bf06c5c07faf9d8531d3745ca1f032b9205a0b
Mark Andrews <mjandrews.net@gmail.com>
2015-09-19 11:29:26 +0100

    Add jags rbf regression model for growth curves
    Model eye tracking growth curve data. One predictor.

---
+++ nonlinear_regression/gca_rbf.1.R
@@ -0,0 +1,40 @@
+y.stretch <- function(y, stretch.factor){
+  y.range.length <- max(y) - min(y)
+  y.center <- min(y) + y.range.length/2
+  ylims <- y.center + c(-1, 1) * y.range.length/2 * stretch.factor
+  return(ylims)
+}
+
+library(rjags)
+
+load('Examples.Rdata')
+
+df <- aggregate(meanFix ~ Time, 
+                data=subset(FunctThemePts, Object=='Target'), 
+                FUN=mean)
+                
+centers <- seq(min(df$Time), max(df$Time), by=100)
+K <- length(centers)
+
+time <- df$Time
+fixation <- df$meanFix
+N <- length(time)
+
+M <- jags.model('gca_rbf.1.jags', 
+                data=list('x'=time, 
+                          'y'=fixation, 
+                          'centers'=centers,
+                          'N'=N, 
+                          'K'=K),
+                n.chains = 3)
+
+update(M, 100000)
+
+S <- coda.samples(M, variable.names = c('width', 'sigma', 'w.sigma'), n.iter = 10000)
+S.mu <- coda.samples(M, variable.names = c('mu'), n.iter = 10000)
+Q <- summary(S.mu)
+
+plot(time, Q$quantiles[,3], ylim=y.stretch(Q$quantiles[,3], 1.25), type='l', col='blue')
+lines(time, Q$quantiles[,1], type='p', col='red')
+lines(time, Q$quantiles[,5], type='p', col='red')
+points(time, fixation, pch=20, cex=0.5)
