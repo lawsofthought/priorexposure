616ce4f70fdf1403fd311d8ed5cfd238693a15c5
Mark Andrews <andrews@mjandrews.net>
2016-04-01 02:32:49 +0100

    Add a simple demonstration of model inference
    In coin example, inference posterior probability of the model.

---
+++ code/simple_models/coin_toss_bf.R
@@ -0,0 +1,27 @@
+library(rjags)
+
+
+N <- 250
+alpha <- 1.0
+beta <- 1.0
+
+y <- c(rep(0, N-139), rep(1, 139))
+n <- sum(y)
+
+
+
+M <- jags.model('coin_toss_bf.jags', 
+                data = list('y'=y, 'N'=N, 'alpha'=alpha, 'beta'=beta),
+                n.chains = 3)
+
+update(M, 10000)
+
+S <- coda.samples(M, 
+                  variable.names = c('x'),
+                  n.iter=10000)
+
+# Examine results 
+summary(S)
+
+posterior.of.null <- mean(c(S[[1]], S[[2]], S[[3]])==2)
+null.posterior.odds <- posterior.of.null/(1-posterior.of.null)
